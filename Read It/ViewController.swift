2e229e81b3dbe478b4ab662f7f41bfc02f2da5a5
Rui Li <orui@outlook.com>
2016-09-15 17:42:00 -0700

    working

--- Read It/ViewController.swift
+++ Read It/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, XMLParserDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MWFeedParserDelegate {
+    
 
     
     // outlets
@@ -17,237 +18,95 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     
     
-    // xml parser
-    var myParser: XMLParser = XMLParser()
-    
-    // rss records
-    var rssRecordList : [RSSInfo] = [RSSInfo]()
-    var rssRecord : RSSInfo?
-    var isTagFound = [ "item": false , "title":false, "date": false ,"link":false]
-    
+    var items = [MWFeedItem]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
-        
-        // set tableview delegate
         self.myTableView.dataSource = self
         self.myTableView.delegate = self
     }
-    override func viewDidDisappear(_ animated: Bool) {
-        //if empty then load data
-        if self.rssRecordList.isEmpty {
-            self.loadRSSData()
-        }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        request()
     }
     
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-    
-    // MARK: - Table view dataSource and Delegate
-    
-    // return number of section within a table
-    private func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+    func request() {
+        let url = URL(string: RSS_FEED_URL)
+        let feedParser = MWFeedParser(feedURL: url);
+        feedParser?.delegate = self
+        feedParser?.parse()
     }
     
-    // return row height
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
+    // MARK: -  MWFeed delegates
+    func feedParserDidStart(_ parser: MWFeedParser!) {
+        SVProgressHUD.show()
+        self.items = [MWFeedItem]()
+    }
+    func feedParserDidFinish(_ parser: MWFeedParser!) {
+        SVProgressHUD.dismiss()
+    }
+    func feedParser(_ parser: MWFeedParser!, didParseFeedInfo info: MWFeedInfo!) {
+        print(info)
+        self.title = info.title
+    }
+    func feedParser(_ parser: MWFeedParser!, didParseFeedItem item: MWFeedItem!) {
+        print(item)
+        self.items.append(item)
     }
     
-    // return how may records in a table
+    // MARK: - Table View delegates
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.rssRecordList.count
+        return self.items.count
     }
-    
-    // return cell
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        // collect reusable cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "rssCell", for: indexPath as IndexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "rssCell", for: indexPath)
         
         // find record for current cell
-        let thisRecord : RSSInfo  = self.rssRecordList[indexPath.row]
+        let thisRecord : MWFeedItem  = self.items[indexPath.row]
         
         // set value for main title and detail tect
         cell.textLabel?.text = thisRecord.title
-        cell.detailTextLabel?.text = thisRecord.date
-        
+        cell.detailTextLabel?.text = thisRecord.summary
+        self.configureCell(cell: cell, atIndexPath: indexPath as IndexPath)
         // return cell
         return cell
     }
-    
-    private func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: IndexPath) {
-       // self.performSegue(withIdentifier: "segueShowDetails", sender: self)
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = self.items[indexPath.row] as MWFeedItem
+        let con = KINWebBrowserViewController()
+        let url = URL(string: item.link)
+        con.load(url)
+        self.navigationController?.pushViewController(con, animated: true)
     }
     
-   
-    // MARK: - NSXML Parse delegate function
-    
-    // start parsing document
-    private func parserDidStartDocument(parser: XMLParser) {
-        // start parsing
-    }
-    
-    // element start detected
-    private func parser(parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
-        
-        if elementName == "item" {
-            self.isTagFound["item"] = true
-            self.rssRecord = RSSInfo()
-            
-        }else if elementName == "title" {
-            self.isTagFound["title"] = true
-            
-        }else if elementName == "link" {
-            self.isTagFound["link"] = true
-            
-        }else if elementName == "date" {
-            self.isTagFound["date"] = true
-        }
-        
-    }
-    
-    // characters received for some element
-    
-    func parser(_ parser: XMLParser, foundCharacters string: String) {
-        
-        if isTagFound["title"] == true {
-            self.rssRecord?.title += string
-            
-        }else if isTagFound["link"] == true {
-            self.rssRecord?.link += string
-            
-        }else if isTagFound["date"] == true {
-            self.rssRecord?.date += string
-        }
-        
-    }
-    
-    // element end detected
-    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        
-        if elementName == "item" {
-            self.isTagFound["item"] = false
-            self.rssRecordList.append(self.rssRecord!)
-            
-        }else if elementName == "title" {
-            self.isTagFound["title"] = false
-            
-        }else if elementName == "link" {
-            self.isTagFound["link"] = false
-            
-        }else if elementName == "date" {
-            self.isTagFound["date"] = false
-        }
-    }
-    
-    // end parsing document
-    func parserDidEndDocument(_ parser: XMLParser) {
-        
-        //reload table view
-        self.myTableView.reloadData()
-        
-        // stop spinner
-        self.spinner.stopAnimating()
-    }
-    
-    // if any error detected while parsing.
-    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
-        
-        //  stop animation
-        self.spinner.stopAnimating()
-        
-        // show error message
-        self.showAlertMessage(alertTitle: "Error", alertMessage: "Error while parsing xml.")
-    }
-    
-    
-    
-    
-    // MARK: - Utility functions
-    
-    // load rss and parse it
-    private func loadRSSData(){
-        
-        if let rssURL = NSURL(string: RSS_FEED_URL) {
-            
-            // start spinner
-            self.spinner.startAnimating()
-            
-            // fetch rss content from url
-            self.myParser = XMLParser(contentsOf: rssURL as URL)!
-            
-            // set parser delegate
-            self.myParser.delegate = self
-            self.myParser.shouldResolveExternalEntities = false
-            
-            // start parsing
-            self.myParser.parse()
-        }
-        
-    }
-    
-    // show alert with ok button
-    private func showAlertMessage(alertTitle: String, alertMessage: String ) -> Void {
-        
-        // create alert controller
-        let alertCtrl = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.alert) as UIAlertController
-        
-        // create action
-        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler:
-            { (action: UIAlertAction) -> Void in
-                // you can add code here if needed
-        })
-        
-        // add ok action
-        alertCtrl.addAction(okAction)
-        
-        // present alert
-        self.present(alertCtrl, animated: true, completion: { (void) -> Void in
-            // you can add code here if needed
-        })
-    }
-    
-    
-    
-    
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    /**
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        
-        if segue.identifier == "segueShowDetails" {
-            
-            // find index path for selected row
-            let selectedIndexPath : [NSIndexPath] = self.myTableView.indexPathsForSelectedRows! as [NSIndexPath]
-            
-            // deselect the selected row
-            self.myTableView.deselectRowAtIndexPath(selectedIndexPath[0], animated: true)
-            
-            // create destination view controller
-            let destVc = segue.destinationViewController as! DetailsViewController
-            
-            // set title for next screen
-            destVc.navigationItem.title = self.rssRecordList[selectedIndexPath[0].row].title
-            
-            // set link value for destination view controller
-            destVc.link = self.rssRecordList[selectedIndexPath[0].row].link
-            
-        }
-
-        
-    }
+    func configureCell(cell: UITableViewCell, atIndexPath indexPath: IndexPath) {
+        /**
+        let item = self.items[indexPath.row] as MWFeedItem
+        cell.textLabel?.text = item.title
+        cell.textLabel?.font = UIFont.systemFontOfSize(14.0)
+        cell.textLabel?.numberOfLines = 0
+     
+        let projectURL = item.link.componentsSeparatedByString("?")[0]
+        let imgURL: URL? = URL(string: projectURL + "/cover_image?style=200x200#")
+        cell.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        cell.imageView?.setImageWithURL(imgURL, placeholderImage: UIImage(named: "logo.png"))
  **/
+ 
+    }
+ 
+
 }
 
--- Read It/common.swift
+++ Read It/common.swift
@@ -10,4 +10,4 @@ import Foundation
 
 
 // RSS Feed URL
-let RSS_FEED_URL = "http://www.prashantmangukiya.com/feed/"
+let RSS_FEED_URL = "http://feeds.reuters.com/reuters/MostRead"
